2352e0ed892f20cbc1e6d238ffb30221e3c7facd
Finomnis <finomnis@gmail.com>
2023-03-26 20:13:32 +0200

    Add dummy renderer

--- effects-1d-frontend-simulator/src/main.rs
+++ effects-1d-frontend-simulator/src/main.rs
@@ -2,6 +2,25 @@
 #![deny(missing_docs)]
 #![deny(unsafe_code)]
 
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+        transform: Transform::default().with_scale(Vec3::splat(512.)),
+        material: materials.add(ColorMaterial::from(Color::GREEN)),
+        ..default()
+    });
 }
