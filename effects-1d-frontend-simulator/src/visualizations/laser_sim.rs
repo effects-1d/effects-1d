080e3ccaeaa4d3a050961e06686ebdb296b0d2e9
Finomnis <finomnis@gmail.com>
2023-03-30 00:18:05 +0200

    Update laser_sim.rs to be identical to ledstrip_sim.rs

--- effects-1d-frontend-simulator/src/visualizations/laser_sim.rs
+++ effects-1d-frontend-simulator/src/visualizations/laser_sim.rs
@@ -4,7 +4,15 @@ use super::WidgetMaterial;
 
 #[derive(AsBindGroup, TypeUuid, Debug, Clone)]
 #[uuid = "a71be379-cac5-4204-8dbd-33982a9e1a60"]
-pub struct LaserSimMaterial {}
+pub struct LaserSimMaterial {
+    #[storage(0, read_only)]
+    pub effect_data: Vec<u32>,
+
+    #[uniform(1)]
+    pub widget_size: Vec2,
+
+    pub rel_size: Vec2,
+}
 
 impl Material2d for LaserSimMaterial {
     fn fragment_shader() -> ShaderRef {
@@ -13,17 +21,17 @@ impl Material2d for LaserSimMaterial {
 }
 
 impl WidgetMaterial for LaserSimMaterial {
-    fn new(_rel_size: Vec2, window_size: Vec2) -> Self {
+    fn new(rel_size: Vec2, window_size: Vec2) -> Self {
         let mut this = Self {
-            // effect_data: vec![0],
-            // widget_size: default(),
-            // rel_size,
+            effect_data: vec![0],
+            widget_size: default(),
+            rel_size,
         };
         this.update_window_size(window_size);
         this
     }
 
-    fn update_window_size(&mut self, _window_size: Vec2) {
-        //self.widget_size = self.rel_size.mul(window_size);
+    fn update_window_size(&mut self, window_size: Vec2) {
+        self.widget_size = self.rel_size * window_size;
     }
 }
