9e045442c86f6a7e5f892e0791e72db2bfac5d09
Finomnis <finomnis@gmail.com>
2023-04-09 18:03:51 +0200

    Attempt to add an effect

--- effects-1d/src/lib.rs
+++ effects-1d/src/lib.rs
@@ -2,3 +2,17 @@
 #![deny(missing_docs)]
 #![deny(unsafe_code)]
 #![no_std]
+
+// Adds effect submodules and exports the effect types
+macro_rules! export_effects {
+    ($($name: ident),*) => {
+        $(
+            ::paste::paste!{
+                mod [<$name:snake>];
+                pub use [<$name:snake>]::$name;
+            }
+        ),*
+    }
+}
+
+export_effects!(BlinkingStripes);
