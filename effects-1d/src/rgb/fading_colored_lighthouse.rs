db112e2c8a641dbe1f1b4235a9922ea7a65b4997
Finomnis <finomnis@gmail.com>
2023-04-26 18:33:17 +0200

    Rework FadingColoredLighthouse

--- effects-1d/src/rgb/fading_colored_lighthouse.rs
+++ effects-1d/src/rgb/fading_colored_lighthouse.rs
@@ -1,5 +1,5 @@
 use effects_1d_common::{
-    color::{self, RGB},
+    color::{self, BlendableColor, Color},
     effects::{BeatBasedEffect, BeatInfo, BlendMode, EffectState, FrameBufferRef},
     errors::RenderError,
     random::{EffectRng, Rng},
@@ -7,69 +7,43 @@ use effects_1d_common::{
 };
 
 #[derive(Debug)]
-pub struct FadingColoredLighthouse {
+struct Stripe {
     line_width: f32,
-    cycle: MultiBeatCycle,
-    rng: EffectRng,
     pos_start: f32,
     pos_end: f32,
-    // left_to_right: bool,
     color: color::RGB,
+    cycle: MultiBeatCycle,
 }
+impl Stripe {
+    pub fn new(offset: u16, line_width: f32, cycle_length: u16, start_beat: i32) -> Self {
+        Self {
+            line_width,
+            cycle: MultiBeatCycle::new(cycle_length, start_beat + i32::from(offset)),
+            pos_start: Default::default(),
+            pos_end: Default::default(),
+            color: Default::default(),
+        }
+    }
 
-impl FadingColoredLighthouse {
-    fn regenerate(&mut self) {
+    fn regenerate(&mut self, rng: &mut EffectRng) {
         let range = 1.0 - self.line_width;
 
-        let pos1 = range * self.rng.gen::<f32>();
-        let pos2 = range * self.rng.gen::<f32>();
+        self.pos_start = range * rng.gen::<f32>();
+        self.pos_end = range * rng.gen::<f32>();
 
-        // if self.left_to_right == (pos1 > pos2) {
-        //     self.pos_start = pos1;
-        //     self.pos_end = pos2;
-        // } else {
-        //     self.pos_start = pos2;
-        //     self.pos_end = pos1;
-        // }
-
-        self.pos_start = pos1;
-        self.pos_end = pos2;
-
-        // self.left_to_right = !self.left_to_right;
-
-        self.color = color::hsv8(self.rng.gen::<f32>() * 360.0, 1.0, 1.0);
-    }
-}
-
-impl BeatBasedEffect for FadingColoredLighthouse {
-    type Color = color::RGB;
-
-    fn init(_resolution_hint: Option<u32>, start_beat: i32) -> Self {
-        let mut this = Self {
-            line_width: 0.1,
-            cycle: MultiBeatCycle::new(4, start_beat),
-            rng: EffectRng::new(),
-            pos_start: 0.0,
-            pos_end: 0.0,
-            // left_to_right: true,
-            color: RGB::new(0, 0, 0),
-        };
-
-        this.regenerate();
-
-        this
+        self.color = color::hsv8(rng.gen::<f32>() * 360.0, 1.0, 1.0);
     }
 
-    fn render_frame(
-        &mut self,
-        framebuffer: &mut dyn FrameBufferRef<color::RGB>,
-        _d_t: f32,
-        beat: BeatInfo,
-    ) -> Result<EffectState, RenderError> {
+    pub fn update(&mut self, beat: BeatInfo, rng: &mut EffectRng) {
         self.cycle.update(beat);
-
         if self.cycle.is_new_cycle() {
-            self.regenerate();
+            self.regenerate(rng);
+        }
+    }
+
+    pub fn render(&self, framebuffer: &mut dyn FrameBufferRef<color::RGB>) {
+        if self.cycle.cycle_number() < 0 {
+            return;
         }
 
         let progress = self.cycle.cycle_progress();
@@ -82,15 +56,54 @@ impl BeatBasedEffect for FadingColoredLighthouse {
             2.0 * (1.0 - progress)
         };
 
-        framebuffer.draw_smooth(
-            pos,
-            pos + self.line_width,
-            self.color,
-            BlendMode::Alpha(brightness),
-        );
+        let color = color::RGB::zero().elementwise_lerp(self.color, brightness);
+
+        framebuffer.draw_smooth(pos, pos + self.line_width, color, BlendMode::Add);
+    }
+}
+
+#[derive(Debug)]
+pub struct FadingColoredLighthouse {
+    rng: EffectRng,
+    stripes: [Stripe; 3],
+}
+
+impl BeatBasedEffect for FadingColoredLighthouse {
+    type Color = color::RGB;
+
+    fn init(_resolution_hint: Option<u32>, start_beat: i32) -> Self {
+        let line_width = 0.1;
+        let cycle_length = 6;
+
+        let mut this = Self {
+            rng: EffectRng::new(),
+            stripes: [
+                Stripe::new(0, line_width, cycle_length, start_beat),
+                Stripe::new(2, line_width, cycle_length, start_beat),
+                Stripe::new(4, line_width, cycle_length, start_beat),
+            ],
+        };
+
+        for stripe in &mut this.stripes {
+            stripe.regenerate(&mut this.rng)
+        }
+
+        this
+    }
+
+    fn render_frame(
+        &mut self,
+        framebuffer: &mut dyn FrameBufferRef<color::RGB>,
+        _d_t: f32,
+        beat: BeatInfo,
+    ) -> Result<EffectState, RenderError> {
+        for stripe in &mut self.stripes {
+            stripe.update(beat, &mut self.rng);
+            stripe.render(framebuffer);
+        }
 
         Ok(EffectState {
-            idle: self.cycle.is_last_beat_of_cycle(),
+            idle: self.stripes[0].cycle.is_last_beat_of_cycle(),
         })
     }
 }
