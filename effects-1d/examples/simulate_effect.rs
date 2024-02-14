a61e3e5bca2b643f5060e792dcec351326e2c40f
Finomnis <finomnis@gmail.com>
2024-02-14 14:29:37 +0100

    Add logging to effects simulator

--- effects-1d/examples/simulate_effect.rs
+++ effects-1d/examples/simulate_effect.rs
@@ -5,5 +5,12 @@ use effects_1d::{binary::*, binary_rgb::*, calibration::*, monochrome::*, rgb::*
 use effects_1d_simulator::SimulateEffect;
 
 fn main() {
-    RandomBlinkingPixels::simulate();
+    tracing_subscriber::fmt()
+        // all spans/events with a level higher than TRACE (e.g, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(tracing::Level::DEBUG)
+        // sets this to be the default, global collector for this application.
+        .init();
+
+    RotatingLinesFilled::simulate();
 }
